1eb440f7342d2eaf632b08fe7c3b8fff7c995247
oofish8888 <77589042+oofish8888@users.noreply.github.com>
2021-02-18 17:06:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.loadPlugin(cmd)
 
 bot.on('login',function(){
 	console.log("Logged In")
-	bot.chat("hello");
+	bot.chat("/register I@MANdude123 I@MANdude123");
 });
 
 bot.on('time', function(time) {
